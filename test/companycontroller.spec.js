948241aaa56ac17daac8cf42c3b792d4df955eae
Zachary Boldyga <zach@scalabull.com>
2017-09-19 18:13:06 -0700

    mocha testing added. nyc added for istanbul support.

---
+++ test/companycontroller.spec.js
@@ -0,0 +1,11 @@
+let mocha = require('mocha');
+let expect = require('chai').expect;
+let companyController = require('../api/controllers/companymongo.controller');
+
+
+describe('Company Controller', function() {
+  describe('#exportedControllerFunctionsHere', function() {
+
+
+  });
+});
